d48cf35f9419a77fa2e7b7d9daf3c8deddfdb280
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-03-10 23:51:03 +0530

    Create NewFile.java
    remote file

---
+++ src/main/java/SeleniumSessions/NewFile.java
@@ -0,0 +1,14 @@
+package SeleniumSessions;
+
+public class NewFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void login() {
+		System.out.println("login");
+	}
+
+}
