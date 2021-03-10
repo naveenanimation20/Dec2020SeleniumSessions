78c9c8cd8e0cd573f6481319b810a5a010d9a496
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-03-11 00:02:24 +0530

    Create Nested.java

---
+++ src/main/java/SeleniumSessions/Nested.java
@@ -0,0 +1,14 @@
+package SeleniumSessions;
+
+public class Nested {
+
+	public static void main(String[] args) {
+
+		System.out.println("remote");
+	}
+	
+	public void login() {
+		System.out.println("remote login");
+	}
+
+}
