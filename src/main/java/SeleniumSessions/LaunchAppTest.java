98479ecdf03fa1e98b39db247e9946fc346d9e1c
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-03-11 08:37:44 +0530

    updated code

--- src/main/java/SeleniumSessions/LaunchAppTest.java
+++ src/main/java/SeleniumSessions/LaunchAppTest.java
@@ -5,13 +5,14 @@ public class LaunchAppTest {
 	public static void main(String[] args) {
 		System.out.println("Remote -- launch app");
 	}
-	
+
 	public void launch() {
 		System.out.println("Remote - launch");
 	}
-  
-  public void closeApp() {
+
+	public void closeApp() {
 		System.out.println("Remote - close app");
+
 	}
 
 }
