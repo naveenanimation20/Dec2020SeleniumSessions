321f602d6d8a057f5a40502e8929512959fd8c63
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-03-11 08:19:37 +0530

    Create NewMerge.java

---
+++ src/main/java/SeleniumSessions/NewMerge.java
@@ -0,0 +1,15 @@
+package SeleniumSessions;
+
+public class NewMerge {
+
+	public static void main(String[] args) {
+
+		System.out.println("remote -- merge");
+		
+	}
+	
+	public void login() {
+		System.out.println("remote--login");
+	}
+
+}
