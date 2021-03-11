756c76981f7de69bf124e5cb6cb2fd7e31a5caaf
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-03-11 08:30:32 +0530

    Create LaunchAppTest.java

---
+++ src/main/java/SeleniumSessions/LaunchAppTest.java
@@ -0,0 +1,17 @@
+package SeleniumSessions;
+
+public class LaunchAppTest {
+
+	public static void main(String[] args) {
+		System.out.println("Remote -- launch app");
+	}
+	
+	public void launch() {
+		System.out.println("Remote - launch");
+	}
+  
+  public void closeApp() {
+		System.out.println("Remote - close app");
+	}
+
+}
